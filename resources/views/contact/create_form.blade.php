90d7d4634409e85651d53da69a8d6697289c9d8d
gabrielvexe <gabriel.vitorsb323@gmail.com>
2022-04-21 15:55:32 -0300

    add value to persist values

--- resources/views/contact/create_form.blade.php
+++ resources/views/contact/create_form.blade.php
@@ -18,15 +18,15 @@
 
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input class="form-control" name="name" id="name" type="text" placeholder="Jorge Alfasoft">
+            <input class="form-control" name="name" id="name" type="text" placeholder="Jorge Alfasoft" value="{{old('name')}}">
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email Address</label>
-            <input class="form-control" name="email" id="email" type="email" placeholder="example@mail.com">
+            <input class="form-control" name="email" id="email" type="email" placeholder="example@mail.com" value="{{old('email')}}">
         </div>
         <div class="mb-3">
             <label for="contact" class="form-label">Contact</label>
-            <input class="form-control" name="contact" id="contact" type="text" placeholder="Ex: 999999999">
+            <input class="form-control" name="contact" id="contact" type="text" placeholder="Ex: 999999999" value="{{old('contact')}}">
         </div>
         <button class="btn btn-primary" type="submit">Salvar</button>
         <a href="{{ route('contact.index') }}" class="btn btn-secondary">Back</a>
