9d60214e8b022b80d7a4fd201d958014c8b6c955
gabrielvexe <gabriel.vitorsb323@gmail.com>
2022-04-21 15:24:48 -0300

    add form to create contact

---
+++ resources/views/contact/create_form.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+    <h3 class="mb-3">Create Contact</h3>
+
+    @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+                <li> {{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+    <form method="POST" action="/contact/store">
+        @csrf
+
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input class="form-control" name="name" id="name" type="text" placeholder="Jorge Alfasoft">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email Address</label>
+            <input class="form-control" name="email" id="email" type="email" placeholder="example@mail.com">
+        </div>
+        <div class="mb-3">
+            <label for="contact" class="form-label">Contact</label>
+            <input class="form-control" name="contact" id="contact" type="text" placeholder="Ex: 999999999">
+        </div>
+        <button class="btn btn-primary" type="submit">Salvar</button>
+        <a href="{{ route('contact.index') }}" class="btn btn-secondary">Back</a>
+    </form>
+@endsection
